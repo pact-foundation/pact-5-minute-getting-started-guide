0cf9b27d91018178707b04467ee6a5daef3805b2
Yousaf Nabi <you@saf.dev>
2023-05-02 17:12:06 +0100

    chore: replit config

--- replit.nix
+++ replit.nix
@@ -1,10 +1,10 @@
 { pkgs }: {
 	deps = [
-		pkgs.python39Full
-    pkgs.nodejs-16_x
-          pkgs.nodePackages.typescript-language-server
-          pkgs.yarn
-          pkgs.replitPackages.jest
-    pkgs.jq
+	pkgs.python39Full
+      pkgs.nodejs-16_x
+      pkgs.nodePackages.typescript-language-server
+      pkgs.yarn
+      pkgs.replitPackages.jest
+      pkgs.jq
 	];
 }
